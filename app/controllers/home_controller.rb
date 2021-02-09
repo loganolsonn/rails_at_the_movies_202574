2b28ff5b9efbf19186797ca1c2c3197b3f9e1f7f
Logan Olson <nolson93@academic.rrc.ca>
2021-02-09 10:53:11 -0600

    Refactored the active record top 10 production companies to the model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,7 @@ class HomeController < ApplicationController
     @movies = Movie.includes(:production_company)
                    .order("average_vote DESC")
                    .limit(10)
-    @production_companies = ProductionCompany.select("production_companies.*")
-                                             .select("COUNT(production_companies.id) as movie_count")
-                                             .joins(:movies)
-                                             .group("production_companies.id")
-                                             .order("movie_count DESC")
+    @production_companies = ProductionCompany.ordered_by_movies
                                              .limit(10)
   end
 end
